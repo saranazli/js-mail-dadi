48f561bf6e15da38be04367bc3bdb6a560daf18a
s4r44d43 <sararez@live.it>
2024-02-23 15:35:09 +0100

    [start] repo

---
+++ README.md
@@ -0,0 +1,25 @@
+esercizio : mail e dadi
+===
+due esercizi:
+mail, il programma chiede la mail all'utente e confronta la sua validità
+dadi, il programma estre due umeri, uno per il giocatore uno per il pc e proclama il vincitore
+## Svolgimento
+
+Esercizio Mail
+
+1. con prompt chiedo la mail dell'utente
+2. in un arrey ho una lista di email che possono accedere
+3. confronto il ruisultato del prompt con i dati dell'arrey
+4. con una condizione (if) accetto le mail incluse nell'arrey e gli    collego un messaggio
+5. a suo interno ci sarà la condizione (else) e le mail non accettate le collego ad un altro messaggio
+6. stampo l'esito della condizione che si verifica relativa alla ail inserita
+
+
+// esercizio Dadi
+
+1. creo un ciclo che si ripete due volte
+2. al suo interno creo un estrattore di numero random da 1 a 6 per una variabile che sarà il giocatore
+3. stessa cosa per una variabile che sarà il computer
+4. creo un istruzione condizionale in cui confronto i numeri dati
+5. il valore più alto sarà la mia variabile vincitrice
+
---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+
+/* 
+1 Mail
+  Chiedi all’utente la sua email,
+  controlla che sia nella lista di chi può accedere,
+  stampa un messaggio appropriato sull’esito del controllo.
+
+2  Gioco dei dadi
+  Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+  Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+// esercizio Mail
+
+/*
+1 con prompt chiedo la mail dell'utente
+2 in un arrey ho una lista di email che possono accedere
+3 confronto il ruisultato del prompt con i dati dell'arrey
+4 con una condizione (if) accetto le mail incluse nell'arrey e gli    collego un messaggio
+5 a suo interno ci sarà la condizione (else) e le mail non accettate le collego ad un altro messaggio
+6 stampo l'esito della condizione che si verifica relativa alla ail inserita
+*/
+
+// esercizio Dadi
+
+/*
+
+1 creo un ciclo che si ripete due volte
+2 al suo interno creo un estrattore di numero random da 1 a 6 per una variabile che sarà il giocatore
+3 stessa cosa per una variabile che sarà il computer
+4 creo un istruzione condizionale in cui confronto i numeri dati
+5 il valore più alto sarà la mia variabile vincitrice
